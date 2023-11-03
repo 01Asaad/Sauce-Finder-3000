ada9a5c7951fbe43a0d7dc2ef5f92a4c48bbf5cf
Asaad Ali <asaad757.asad@gmail.com>
2023-11-03 22:32:23 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// Randomly change the color of "TOMATO" every second
+const randomColorText = document.getElementById('randomColorText');
+const colors = ['red', 'blue', 'green', 'orange', 'purple'];
+let index = 0;
+
+setInterval(() => {
+    randomColorText.style.color = colors[index];
+    index = (index + 1) % colors.length;
+}, 1000);
